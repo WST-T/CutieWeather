acff063007f9dcea5a31a9b548b4f09b6d34e7b1
wsTnT <wstntgg@gmail.com>
2024-10-23 06:16:24 +0200

    Add the CutieWeather Project.

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import './App.css'
+
+import SearchBar from './components/SearchBar'
+import MiddleInformation from './components/MiddleInformation'
+import BottomInformation from './components/BottomInformation'
+import clouds_icon from '/public/assets/clouds.png'
+import clear_icon from '/public/assets/clear.png'
+import drizzle_icon from '/public/assets/drizzle.png'
+import mist_icon from '/public/assets/mist.png'
+import rain_icon from '/public/assets/rain.png'
+import snow_icon from '/public/assets/snow.png'
+import fog_icon from '/public/assets/fog.png'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+function App() {
+  const [city, setCity] = useState('Paris')
+  const [information, setInformation] = useState({})
+
+  useEffect(() => {
+    const icon_mapping = {
+      "Clouds": clouds_icon,
+      "Clear": clear_icon,
+      "Drizzle": drizzle_icon,
+      "Mist": mist_icon,
+      "Rain": rain_icon,
+      "Snow": snow_icon,
+      "Fog": fog_icon,
+    }
+    const getWeatherData = async () => {
+      try {
+        const api_key = import.meta.env.VITE_WEATHER_API_KEY
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${api_key}`);
+        if (!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+        const json = await response.json();
+        const sunrise_time = new Date((json.sys.sunrise + json.timezone - 3600) * 1000);
+        const sunset_time = new Date((json.sys.sunset + json.timezone - 3600) * 1000);
+
+        setInformation({
+          weather_icon: icon_mapping[json.weather[0].main],
+          weather_degree: Math.round(parseInt(json.main.temp)),
+          weather_city: json.name,
+          humidity: json.main.humidity,
+          wind: json.wind.speed,
+          sunrise: sunrise_time.getHours() + ":" + sunrise_time.getMinutes(),
+          sunset: sunset_time.getHours() + ":" + sunset_time.getMinutes(),
+        })
+        console.log(json);
+      } catch (error) {
+        console.log('There was an error', error)
+      }
+    }
+    getWeatherData()
+  }, [city])
+
+
+  return (
+    <div className='container'>
+      <div className='card'>
+        <SearchBar setCity={setCity} />
+        <MiddleInformation weather_icon={information.weather_icon} weather_degree={information.weather_degree} weather_city={information.weather_city} />
+        <BottomInformation humidity={information.humidity} wind={information.wind} sunrise={information.sunrise} sunset={information.sunset} />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/BottomInformation.jsx
@@ -0,0 +1,42 @@
+import './BottomInformation.css'
+import humidity_icon from "/public/assets/humidity.png"
+import wind_icon from "/public/assets/wind.png"
+import sunrise_icon from "/public/assets/sunrise.png"
+import sunset_icon from "/public/assets/sunset.png"
+
+const BottomInformation = ({humidity, wind, sunrise, sunset}) => {
+  return (
+    <div className='bottom-info'>
+      <div className='elements'>
+        <img src={humidity_icon} alt='' />
+        <div className='data'>
+          <div className='humidity-info'>{humidity}</div>
+          <div className='text'>Humidity</div>
+        </div>
+      </div>
+      <div className='elements'>
+        <img src={wind_icon} alt='' />
+        <div className='data'>
+          <div className='wind-info'>{wind}</div>
+          <div className='text'>Wind Speed</div>
+        </div>
+      </div>
+      <div className='elements'>
+        <img src={sunrise_icon} alt='' />
+        <div className='data'>
+          <div className='sunrise-info'>{sunrise}</div>
+          <div className='text'>Sunrise Time</div>
+        </div>
+      </div>
+      <div className='elements'>
+        <img src={sunset_icon} alt='' />
+        <div className='data'>
+          <div className='sunset-info'>{sunset}</div>
+          <div className='text'>Sunset Time</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BottomInformation
